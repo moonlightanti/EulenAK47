f296534dff4fa4a8846b14562acd12f309e2766d
Moonlight Dev Team <94982648+moonlightanti@users.noreply.github.com>
2022-05-25 11:02:48 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+version '1.0.0'
+lua54 'yes'
+
+author 'Moonlight Anticheat | Moonlight Development Team'
+
+game 'gta5'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
